eleanorLYJ/rv32emu

Add support for the RV32E base ISA variant
The RV32E variant reduces the general-purpose registers to 16, optimizing
the architecture for resource-constrained embedded microcontrollers.

This addition enhances compatibility with the RISC-V specification and
targets low-power, minimal-complexity systems.

src/riscv.c
@@ -769,10 +769,15 @@ void rv_reset(riscv_t *rv, riscv_word_t pc)
     rv->csr_mtvec = 0;
     rv->csr_cycle = 0;
     rv->csr_mstatus = 0;
-    rv->csr_misa |= MISA_SUPER | MISA_USER | MISA_I;
+    rv->csr_misa |= MISA_SUPER | MISA_USER;
     rv->csr_mvendorid = RV_MVENDORID;
     rv->csr_marchid = RV_MARCHID;
     rv->csr_mimpid = RV_MIMPID;
+#if !RV32_HAS(EXT_E)
+    rv->csr_misa |= MISA_I;
+#else
+    rv->csr_misa |= MISA_E;
+#endif
 #if RV32_HAS(EXT_A)
     rv->csr_misa |= MISA_A;
 #endif

src/syscall.c
@@ -466,8 +466,12 @@ static void syscall_sbi_rst(riscv_t *rv)
 
 void syscall_handler(riscv_t *rv)
 {
-    /* get the syscall number */
+/* get the syscall number */
+#if !RV32_HAS(EXT_E)
     riscv_word_t syscall = rv_get_reg(rv, rv_reg_a7);
+#else
+    riscv_word_t syscall = rv_get_reg(rv, rv_reg_t0);
+#endif
 
     switch (syscall) { /* dispatch system call */
 #define _(name, number)     \

ChinYikMing/rv32emu-wasm

src/riscv.c
@@ -770,10 +770,15 @@ void rv_reset(riscv_t *rv, riscv_word_t pc)
     rv->csr_mtvec = 0;
     rv->csr_cycle = 0;
     rv->csr_mstatus = 0;
-    rv->csr_misa |= MISA_SUPER | MISA_USER | MISA_I;
+    rv->csr_misa |= MISA_SUPER | MISA_USER;
     rv->csr_mvendorid = RV_MVENDORID;
     rv->csr_marchid = RV_MARCHID;
     rv->csr_mimpid = RV_MIMPID;
+#if !RV32_HAS(RV32E)
+    rv->csr_misa |= MISA_I;
+#else
+    rv->csr_misa |= MISA_E;
+#endif
 #if RV32_HAS(EXT_A)
     rv->csr_misa |= MISA_A;
 #endif

src/syscall.c
@@ -466,8 +466,12 @@ static void syscall_sbi_rst(riscv_t *rv)
 
 void syscall_handler(riscv_t *rv)
 {
-    /* get the syscall number */
+/* get the syscall number */
+#if !RV32_HAS(RV32E)
     riscv_word_t syscall = rv_get_reg(rv, rv_reg_a7);
+#else
+    riscv_word_t syscall = rv_get_reg(rv, rv_reg_t0);
+#endif
 
     switch (syscall) { /* dispatch system call */
 #define _(name, number)     \