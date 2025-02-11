690dd4b08e25b0ac27aead6297f0150052f2d047
ChinYikMing <yikming2222@gmail.com>
2025-02-12 01:59:49 +0800

    test

--- .ci/riscv-toolchain-install.sh
+++ .ci/riscv-toolchain-install.sh
@@ -16,8 +16,9 @@ if [[ "$#" == "0" ]] || [[ "$1" != "riscv-collab" ]]; then
     elif [[ ${MACHINE_TYPE} == "aarch64" ]]; then
         TOOLCHAIN_URL=${TOOLCHAIN_REPO}/releases/download/v${GCC_VER}/xpack-riscv-none-elf-gcc-${GCC_VER}-linux-arm64.tar.gz
     elif [[ ${MACHINE_TYPE} == "arm64" ]]; then
-	echo "----------------------------------arm64 xPack toolchain-------------------------------------"
         TOOLCHAIN_URL=${TOOLCHAIN_REPO}/releases/download/v${GCC_VER}/xpack-riscv-none-elf-gcc-${GCC_VER}-darwin-arm64.tar.gz
+	echo "----------------------------------arm64 xPack toolchain-------------------------------------"
+	echo "TOOLCHAIN URL: ${TOOLCHAIN_URL}"
     fi
 else
     UBUNTU_VER=`lsb_release -r | cut -f2`
